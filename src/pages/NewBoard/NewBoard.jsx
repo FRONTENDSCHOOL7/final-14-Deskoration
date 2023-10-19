c9871e1af200f73ef5507a964ac3546c53f188a2
yen <l.yennie.j@gmail.com>
2023-10-20 03:05:59 +0900

    :sparkles: :sparkles: Add 게시글 마크업
    #5 게시글 UI를 위한 기본적인 마크업

--- src/pages/NewBoard/NewBoard.jsx
+++ src/pages/NewBoard/NewBoard.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import * as S from './NewBoard.styled';
 
 const NewBoard = () => {
-    return <div>NewBoard</div>;
+    return (
+        <>
+            {/* 헤더 컴포넌트 불러올 예정 */}
+            <header
+                className="header"
+                style={{ width: '100%', height: '70px' }}
+            >
+                <h4 className="title">게시물 작성</h4>
+            </header>
+            <form>
+                <input type="file" />
+                <textarea></textarea>
+                <button type="submit">올리기</button>
+            </form>
+        </>
+    );
 };
 
 export default NewBoard;

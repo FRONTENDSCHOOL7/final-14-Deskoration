eb82bc0cf081c8641fb7433b80d9feda8347224e
yen <l.yennie.j@gmail.com>
2023-10-20 03:13:33 +0900

    :sparkles: Add file input custom function
    사진을 업로드하기 위한 input태그 커스텀 함수 생성 Fixes:#5

--- src/pages/NewBoard/NewBoard.jsx
+++ src/pages/NewBoard/NewBoard.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import * as S from './NewBoard.styled';
 
 const NewBoard = () => {
+    const hiddenFileInput = useRef(null);
+
+    const handleInputClick = event => {
+        event.preventDefault();
+        hiddenFileInput.current.click();
+    };
+    const handleFileChange = event => {
+        const fileUploaded = event.target.files[0];
+        console.log(fileUploaded);
+    };
     return (
         <>
             {/* 헤더 컴포넌트 불러올 예정 */}
             <header
                 className="header"
-                style={{ width: '100%', height: '70px' }}
+                style={{
+                    width: '100%',
+                    height: '70px',
+                }}
             >
                 <h4 className="title">게시물 작성</h4>
             </header>
             <form>
-                <input type="file" />
+                <button type="button" onClick={handleInputClick}>
+                    사진 추가
+                </button>
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    ref={hiddenFileInput}
+                    style={{ display: 'none' }}
+                />
                 <textarea></textarea>
                 <button type="submit">올리기</button>
             </form>

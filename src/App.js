0b41065d1be18bfa55a62f8bc6c00e75bf0a0962
yen <l.yennie.j@gmail.com>
2023-10-19 16:15:04 +0900

    :lipstick: Add mobile view
    모바일 화면을 기준으로 애플리케이션을 개발하기로 하여 정해진 크기로 기본 틀을 잡아놨습니다. 추후에 라우터가 설정이 되면 라우터스타일컴포넌트로 옮길 예정입니다.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
+// 추후 라우터 설정하면 라우터에 css적용할 예정입니다.
+import './App.css';
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            DESKORATION
-        </ThemeProvider>
+        <div className="app-body">
+            <div className="container">
+                <ThemeProvider theme={theme}>
+                    <GlobalStyle />
+                    DESKORATION
+                </ThemeProvider>
+            </div>
+        </div>
     );
 }
 

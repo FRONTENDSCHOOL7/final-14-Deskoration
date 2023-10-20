50561343df9f27343501f50ad7b6ab8f8fbbeff4
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-20 18:46:49 +0900

    :lipstick: Fix 스타일 theme 적용
    #1 theme 적용하여 컬러와 폰트사이즈 수정

--- src/pages/NewBoard/RegisterForm.styled.jsx
+++ src/pages/NewBoard/RegisterForm.styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-
+import theme from '../../styles/theme';
+console.log({ theme });
 export const ProductRegisterForm = styled.form`
     width: 100%;
     padding: 0 30px;
@@ -11,8 +12,8 @@ export const ProductRegisterForm = styled.form`
 
 export const InputLabel = styled.label`
     display: inline-block;
-    color: #767676;
-    font-size: 12px;
+    color: ${({ theme }) => theme.subFont};
+    font-size: ${({ theme }) => theme.fontSize.sm};
     font-weight: 700;
     margin-bottom: 10px;
 `;
@@ -21,12 +22,12 @@ export const InputText = styled.input`
     width: 100%;
     padding: 2.5px;
     border-style: none;
-    border-bottom: 1px solid #dbdbdb;
+    border-bottom: 1px solid ${({ theme }) => theme.border};
     margin-bottom: 10px;
 
     &:focus {
         outline: none;
-        border-bottom: 1px solid #0f0f0f;
+        border-bottom: 1px solid ${({ theme }) => theme.mainFont};
     }
 `;
 
@@ -35,12 +36,12 @@ export const RegisterButton = styled.button`
     height: 26px;
     color: #fff;
     border-radius: 10px;
-    background-color: #45522b;
+    background-color: ${({ theme }) => theme.main};
 `;
 
 export const Warning = styled.strong`
     display: block;
     font-size: 10px;
-    color: red;
+    color: ${({ theme }) => theme.repo.open};
     margin-bottom: 10px;
 `;

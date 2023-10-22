85dbdff6015ed85c80998299d388243cbaa2a828
yen <l.yennie.j@gmail.com>
2023-10-22 14:35:08 +0900

    :lipstick: Update NewBoard UI
    태그를 추가하기 위한 input 태그와 header 태그 UI 변경 Fixes:#5

--- src/pages/NewBoard/NewBoard.jsx
+++ src/pages/NewBoard/NewBoard.jsx
@@ -1,43 +1,73 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import * as S from './NewBoard.styled';
 
+const NewBoardInFormButton = ({ name, handleClick }) => {
+    return (
+        <S.FileInputButton type="button" onClick={handleClick}>
+            {name}
+        </S.FileInputButton>
+    );
+};
+
 const NewBoard = () => {
+    const [img, setImg] = useState('');
+
     const hiddenFileInput = useRef(null);
 
     const handleInputClick = event => {
         event.preventDefault();
         hiddenFileInput.current.click();
     };
+
     const handleFileChange = event => {
         const fileUploaded = event.target.files[0];
         console.log(fileUploaded);
+        setImg(fileUploaded);
     };
     return (
-        <>
-            {/* 헤더 컴포넌트 불러올 예정 */}
-            <header
-                className="header"
-                style={{
-                    width: '100%',
-                    height: '70px',
-                }}
-            >
-                <h4 className="title">게시물 작성</h4>
-            </header>
-            <S.NewBoarForm>
-                <S.FileInputButton type="button" onClick={handleInputClick}>
-                    사진 추가
-                </S.FileInputButton>
-                <input
-                    type="file"
-                    onChange={handleFileChange}
-                    ref={hiddenFileInput}
-                    style={{ display: 'none' }}
-                />
-                <S.NewBoardTextarea></S.NewBoardTextarea>
-                <button type="submit">올리기</button>
-            </S.NewBoarForm>
-        </>
+        <S.NewBoardContainer>
+            <S.NewBoardHeader>
+                <i>아이콘</i>
+                <h4>게시물 작성</h4>
+            </S.NewBoardHeader>
+            <form>
+                <S.NewBoardFileContainer $hasImg={img}>
+                    <>
+                        <input
+                            type="file"
+                            onChange={handleFileChange}
+                            ref={hiddenFileInput}
+                            style={{ display: 'none' }}
+                        />
+                        {img ? (
+                            <S.ImgControlBox>
+                                <NewBoardInFormButton
+                                    name="사진 변경"
+                                    handleClick={handleInputClick}
+                                />
+                                {/* 추후 상품정보 입력 페이지로 변경되는 함수 handleClick에 추가 */}
+                                <NewBoardInFormButton name="태그 추가" />
+                                <NewBoardInFormButton
+                                    name="사진 삭제"
+                                    handleClick={() => setImg('')}
+                                />
+                            </S.ImgControlBox>
+                        ) : (
+                            <>
+                                <NewBoardInFormButton
+                                    name="사진 추가"
+                                    handleClick={handleInputClick}
+                                />
+                            </>
+                        )}
+                    </>
+                </S.NewBoardFileContainer>
+                <S.NewBoardTextarea />
+                <S.SubmitNewBoardButton type="submit">
+                    올리기
+                </S.SubmitNewBoardButton>
+            </form>
+        </S.NewBoardContainer>
     );
 };
 
--- src/pages/NewBoard/NewBoard.styled.jsx
+++ src/pages/NewBoard/NewBoard.styled.jsx
@@ -1,30 +1,102 @@
 import styled from 'styled-components';
 
-export const NewBoarForm = styled.form`
-    display: flex;
-    flex-direction: column;
-
+export const NewBoardContainer = styled.div`
+    position: relative;
     padding: 0 25px;
 `;
 
-export const FileInputButton = styled.button`
+export const NewBoardHeader = styled.header`
+    display: flex;
+
+    align-items: center;
+    height: 70px;
+
+    font-size: 24px;
+    font-family: 'PreBold';
+`;
+
+export const ImgControlBox = styled.div`
+    position: absolute;
+    left: 0;
+    bottom: 0;
+
+    padding: 15px;
+    border-radius: 0 0 14px 14px;
+
+    text-align: center;
+
+    button {
+        &:first-child,
+        &:last-child {
+            display: none;
+        }
+    }
+
+    width: 100%;
+`;
+
+export const NewBoardFileContainer = styled.div`
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
     height: 400px;
-    border: 1px solid gray;
+
+    border: 1px solid ${props => props.theme.border};
     border-radius: 14px;
     margin-bottom: 20px;
 
+    background: ${props => (props.$hasImg ? '#BF4F74' : 'white')};
+
     &:hover {
-        color: #fff;
-        background-color: #45522b;
+        ${ImgControlBox} {
+            display: flex;
+            justify-content: space-around;
+
+            background: linear-gradient(transparent, rgba(0, 0, 0, 0.54));
+
+            button {
+                &:first-child,
+                &:last-child {
+                    display: block;
+                }
+            }
+        }
+    }
+`;
+export const FileInputButton = styled.button`
+    border: 1px solid ${props => props.theme.border};
+    border-radius: 14px;
+    padding: 10px;
+
+    &:hover {
+        /* color: #fff;
+        background-color: #45522b; */
+        outline: 7px solid ${props => props.theme.main};
     }
 `;
 
 export const NewBoardTextarea = styled.textarea`
+    display: block;
     outline: none;
     resize: none;
 
+    width: 100%;
     height: 200px;
-    border: 1px solid gray;
+
+    border: 1px solid ${props => props.theme.border};
     border-radius: 14px;
     padding: 10px;
+`;
+
+export const SubmitNewBoardButton = styled.button`
+    position: absolute;
+    top: 20px;
+    right: 25px;
+
+    color: ${props => props.theme.point};
+    font-size: 24px;
+    font-family: 'PreBold';
 `;

798e95c879e3f62ce240bb368656d83c77497528
HyeonJin <ocn4091@naver.com>
2023-10-23 15:48:15 +0900

    :sparkles: Add 유저명 검색 기능 구현
    #2 채팅방 유저명으로만 검색 기능 구현

--- src/pages/Chat/ChatListPage/ChatListPage.jsx
+++ src/pages/Chat/ChatListPage/ChatListPage.jsx
@@ -1,20 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './ChatListPage.styled';
+import { profile } from '../../../mock/mockData';
 
 const ChatListPage = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const filteredProfiles = profile.filter(profile =>
+        profile.username.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
+
     return (
         <>
             <S.ChatListPageContainer>
-                <S.ChatlistPageDiv>
-                    <S.StyledSearchIcon />
-                    <S.SearchUsernameInput
-                        type="text"
-                        placeholder="Search user"
-                        // value={searchQuery}
-                        // onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                </S.ChatlistPageDiv>
+                {/* 헤더 부분 */}
+                <S.ChatListHeader>
+                    {/* header button 가져오기 */}
+                    <button>
+                        <img src="/images/icon-backward.jpg" alt="" />
+                    </button>
+                    <h2>Chat</h2>
+                </S.ChatListHeader>
+
+                {/* 메인 부분 */}
+                <S.ChatlistPageMain>
+                    <S.SearchBar>
+                        <S.SearchIcon />
+                        <S.SearchUsernameInput
+                            type="text"
+                            placeholder="Search user"
+                            value={searchQuery}
+                            onChange={e => setSearchQuery(e.target.value)}
+                        />
+                    </S.SearchBar>
+
+                    <S.UserChatList>
+                        {filteredProfiles.map((profile, index) => (
+                            <li key={profile._id}>
+                                <S.UserChatRoom>
+                                    <img
+                                        src={profile.image}
+                                        className="user-img"
+                                        alt=""
+                                    />
+                                    <S.UserSimpleinfo>
+                                        <h2 className="user-name">
+                                            {profile.username}
+                                        </h2>
+                                        <div className="user-msg-time">
+                                            <p className="user-lastMeassage">
+                                                {
+                                                    profile.messages[
+                                                        profile.messages
+                                                            .length - 1
+                                                    ]
+                                                }
+                                            </p>
+                                            <p className="user-date">{`${month}.${day}`}</p>
+                                        </div>
+                                    </S.UserSimpleinfo>
+                                </S.UserChatRoom>
+                            </li>
+                        ))}
+                    </S.UserChatList>
+                </S.ChatlistPageMain>
             </S.ChatListPageContainer>
+            {/* 푸터 부분 */}
+            <S.ChatListPageFooter>
+                {/* 푸터 버튼 가져오기 */}
+                <div className="btn-list">
+                    <button className="btn">home</button>
+                    <button className="btn">feed</button>
+                    <button className="btn">create</button>
+                    <button className="btn">chat</button>
+                    <button className="btn">profile</button>
+                </div>
+            </S.ChatListPageFooter>
         </>
     );
 };

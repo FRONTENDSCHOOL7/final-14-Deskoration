7fd9ae14aaa7fef014ddc1497fb0c7c325ee16e5
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-23 16:21:16 +0900

    :art: Change Marker 컴포넌트 분리
    Fixes:#4 Marker 컴포넌트를 파일 분리

---
+++ src/components/Marker/Marker.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Marker = React.forwardRef((props, markerRef) => {
+    const { onMouseDown, handleMarkerLoad, markerLocation } = props;
+    return (
+        <button>
+            <img
+                src={`${process.env.PUBLIC_URL}/images/marker.svg`}
+                alt="이미지 마커 표시"
+                className="marker"
+                ref={markerRef}
+                onMouseDown={onMouseDown}
+                onLoad={handleMarkerLoad}
+                style={{
+                    position: 'absolute',
+                    left: `${markerLocation.left}%`,
+                    top: `${markerLocation.top}%`,
+                }}
+            />
+        </button>
+    );
+});

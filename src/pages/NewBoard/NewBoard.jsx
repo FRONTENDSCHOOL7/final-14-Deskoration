45db0fb2843b69e0273836264ad3c19b610a2a35
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-23 16:31:43 +0900

    :art: Fix display 속성으로 화면 렌더링
    Related to: #4,#8 조건부 렌더링에서 display 속성으로 화면 렌더링을 컨트롤

--- src/pages/NewBoard/NewBoard.jsx
+++ src/pages/NewBoard/NewBoard.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 import { RegisterForm } from './RegisterForm';
+import { DragNDrop } from './DragNDrop';
 import * as S from './NewBoard.styled';
 
 const NewBoard = () => {
     const [showRegisterForm, setShowRegisterForm] = useState(false);
     const [items, setItems] = useState([]);
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
 
-    const onRegister = () => {
-        setShowRegisterForm(true);
-    };
+    const [data, setData] = useState({
+        id: '',
+        location: {},
+        category: '',
+        productName: '',
+        price: '',
+        store: '',
+        link: '',
+    });
+
+    console.log(items);
+
+    const dragNDropStyle = showRegisterForm ? { display: 'none' } : {};
+    const registerFormStyle = showRegisterForm ? {} : { display: 'none' };
 
     return (
         <>
@@ -18,22 +31,28 @@ const NewBoard = () => {
             >
                 <h4 className="title">사진선택</h4>
             </header>
-            <article style={{ padding: '0 30px' }}>
-                {!showRegisterForm && (
-                    <img
-                        src={`${process.env.PUBLIC_URL}/images/dummyImg.jpg`}
-                        style={{ width: '100%', display: 'block' }}
-                        onClick={onRegister}
-                        alt="사진"
-                    />
-                )}
-                {showRegisterForm && (
-                    <RegisterForm
-                        setShowRegisterForm={setShowRegisterForm}
-                        items={items}
-                        setItems={setItems}
-                    />
-                )}
+            <article style={{ padding: '0 25px' }}>
+                <DragNDrop
+                    displayStyle={dragNDropStyle}
+                    setData={setData}
+                    setOffset={setOffset}
+                    items={items}
+                    setItems={setItems}
+                    onImageClick={() =>
+                        items.length < 5
+                            ? setShowRegisterForm(true)
+                            : alert('상품은 최대 5개까지 추가할 수 있습니다.')
+                    }
+                />
+                <RegisterForm
+                    displayStyle={registerFormStyle}
+                    setShowRegisterForm={setShowRegisterForm}
+                    items={items}
+                    setItems={setItems}
+                    data={data}
+                    setData={setData}
+                    offset={offset}
+                />
             </article>
         </>
     );

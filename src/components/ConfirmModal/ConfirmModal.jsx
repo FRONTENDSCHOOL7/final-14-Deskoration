9384599b5c4add4807af3f71132bd3deea1a764c
yen <l.yennie.j@gmail.com>
2023-10-24 01:51:28 +0900

    :sparkles: Add 컨펌 모달 마크업
    #11 컨펌 모달 마크업

--- src/components/ConfirmModal/ConfirmModal.jsx
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 import * as S from './ConfirmModal.styled';
 
 const ConfirmModal = () => {
-    return <div></div>;
+    return (
+        <S.Dialog>
+            <S.ConfirmModalContainer>
+                <S.ConfirmModalQustion>관련된 물음</S.ConfirmModalQustion>
+                <S.ConfirmModalButtonBox>
+                    <button type="button">yes</button>
+                    <button type="button">no</button>
+                </S.ConfirmModalButtonBox>
+
+                <S.CloseConfirmModalButton type="button">
+                    close
+                </S.CloseConfirmModalButton>
+            </S.ConfirmModalContainer>
+        </S.Dialog>
+    );
 };
 
 export default ConfirmModal;
---
+++ src/components/ConfirmModal/ConfirmModal.styled.jsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const Dialog = styled.dialog``;
+
+export const ConfirmModalContainer = styled.section``;
+
+export const ConfirmModalQustion = styled.p``;
+
+export const ConfirmModalButtonBox = styled.div``;
+
+export const CloseConfirmModalButton = styled.button``;

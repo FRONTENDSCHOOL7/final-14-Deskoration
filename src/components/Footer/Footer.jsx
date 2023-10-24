ef3c0367dccb27abe3ef570df3b6194e5146b03e
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-24 15:08:59 +0900

    :sparkles: Add Footer Layout & Button Components
    #12_1 푸터 레이아웃과 버튼 컴포넌트 생성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Footer.styled';
+import { HomeBtn, BrowseBtn, AddBtn, ChatBtn, UserBtn } from './Buttons';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    const [hover, setHover] = useState({
+        home: false,
+        browse: false,
+        add: false,
+        chat: false,
+        user: false,
+    });
+
+    const handleHover = btnType => {
+        if (btnType === 'home') {
+            setHover(prev => ({
+                ...prev,
+                home: !hover,
+            }));
+        }
+    };
+
+    return (
+        <S.Footer>
+            <HomeBtn hover={hover} handleHover={() => handleHover('home')} />
+            <BrowseBtn hover={hover} handleHover={handleHover} />
+            <AddBtn hover={hover} handleHover={handleHover} />
+            <ChatBtn hover={hover} handleHover={handleHover} />
+            <UserBtn hover={hover} handleHover={handleHover} />
+        </S.Footer>
+    );
 };
 
 export default Footer;

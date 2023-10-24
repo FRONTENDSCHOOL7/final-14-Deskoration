e3bb475562cb67ad22eb7c30c82ad681d12bea1d
yen <l.yennie.j@gmail.com>
2023-10-24 15:08:32 +0900

    :seedling: Add common button component
    #13 공통 버튼을 위한 컴포넌트

---
+++ src/hooks/CommonButton/CommonButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './CommonButton.styled';
+
+const CommonButton = () => {
+    return <button>common button</button>;
+};
+
+export default CommonButton;

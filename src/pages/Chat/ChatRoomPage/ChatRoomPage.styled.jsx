f0d28b321c67e0a21f8a30b260db2c7c08db6804
HyeonJin <ocn4091@naver.com>
2023-10-24 23:11:55 +0900

    :lipstick: Add 채팅창 스타일 구현
    #10 채팅창 스타일 파일 추가

--- src/pages/Chat/ChatRoomPage/ChatRoomPage.styled.jsx
+++ src/pages/Chat/ChatRoomPage/ChatRoomPage.styled.jsx
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
-// export const
+export const ChatRoomPageContainer = styled.div`
+    position: relative;
+    margin: 0 25px;
+`;
+export const ChatListHeader = styled.div`
+    display: flex;
+    width: 310px;
+    height: 70px;
+    padding-top: 20px;
+    font-size: 24px;
+    align-items: center;
+
+    .user-img {
+        width: 50px;
+        height: 50px;
+        border-radius: 100%;
+        margin-left: 8px;
+        margin-right: 8px;
+    }
+`;

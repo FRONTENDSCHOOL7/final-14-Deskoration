84e034255f539cc05dba675c7bc40e763d84c056
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-24 15:09:52 +0900

    :lipstick: Add Footer style
    #12_1 푸터 스타일드 컴포넌트 생성

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const Footer = styled.footer`
+    display: flex;
+    width: 100%;
+    height: 90px;
+    padding: 0 25px;
+    align-items: center;
+    justify-content: space-between;
+    background-color: beige;
+    position: absolute;
+    bottom: 0;
+`;

5760ce97fcf43db0ae176ed902f507807b4ac3b6
yen <l.yennie.j@gmail.com>
2023-10-24 14:47:58 +0900

    :lipstick: Add modal UI
    컨펌 모달 기본 CSS Fixed:#11

--- src/components/ConfirmModal/ConfirmModal.jsx
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './ConfirmModal.styled';
 
 const ConfirmModal = () => {
-    return (
-        <S.Dialog>
-            <S.ConfirmModalContainer>
-                <S.ConfirmModalQustion>관련된 물음</S.ConfirmModalQustion>
-                <S.ConfirmModalButtonBox>
-                    <button type="button">yes</button>
-                    <button type="button">no</button>
-                </S.ConfirmModalButtonBox>
+    const [temp, setTemp] = useState(false);
+    const onTemp = () => setTemp(!temp);
 
-                <S.CloseConfirmModalButton type="button">
-                    close
-                </S.CloseConfirmModalButton>
-            </S.ConfirmModalContainer>
-        </S.Dialog>
+    return (
+        <>
+            <button onClick={onTemp}>modal</button>
+            <S.Dialog open={temp}>
+                <S.ConfirmModalContainer>
+                    <S.ConfirmModalInnerContainer>
+                        <S.ConfirmModalQustion>
+                            관련된 물음
+                        </S.ConfirmModalQustion>
+                        <S.ConfirmModalButtonBox>
+                            <button type="button">yes</button>
+                            <button type="button">no</button>
+                        </S.ConfirmModalButtonBox>
+
+                        <S.CloseConfirmModalButton
+                            type="button"
+                            onClick={onTemp}
+                        >
+                            X아이콘
+                        </S.CloseConfirmModalButton>
+                    </S.ConfirmModalInnerContainer>
+                </S.ConfirmModalContainer>
+            </S.Dialog>
+        </>
     );
 };
 
--- src/components/ConfirmModal/ConfirmModal.styled.jsx
+++ src/components/ConfirmModal/ConfirmModal.styled.jsx
@@ -1,11 +1,55 @@
 import { styled } from 'styled-components';
 
-export const Dialog = styled.dialog``;
+export const Dialog = styled.dialog`
+    position: absolute;
+    top: 0;
+    left: 0;
 
-export const ConfirmModalContainer = styled.section``;
+    width: 100%;
+    height: 100%;
 
-export const ConfirmModalQustion = styled.p``;
+    background: rgba(0, 0, 0, 0.4);
+    backdrop-filter: blur(5px);
+`;
 
-export const ConfirmModalButtonBox = styled.div``;
+export const ConfirmModalContainer = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-export const CloseConfirmModalButton = styled.button``;
+    height: 100%;
+`;
+
+export const ConfirmModalInnerContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background: #fff;
+    text-align: center;
+    border-radius: 14px;
+    padding: 15px;
+    position: relative;
+
+    width: 300px;
+    height: 350px;
+`;
+
+export const ConfirmModalQustion = styled.p`
+    font-size: 24px;
+    font-family: 'PreBold';
+`;
+
+export const ConfirmModalButtonBox = styled.div`
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+`;
+
+export const CloseConfirmModalButton = styled.button`
+    position: absolute;
+    top: 10px;
+    right: 24px;
+`;

69e5314faa39cf58cdf346fa3f8964209f84da18
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-25 15:11:43 +0900

    👌 Fix 초기 마커 위치 조정
    Resolve:#4 class 선택자를 추가하여 width값의 우선순위 조정

--- src/components/Marker/Marker.styled.jsx
+++ src/components/Marker/Marker.styled.jsx
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 export const MarkerPointer = styled.img`
     position: absolute;
-    width: 20px;
+
     height: 20px;
     z-index: 100;
+
+    &.marker {
+        width: 20px;
+    }
 `;

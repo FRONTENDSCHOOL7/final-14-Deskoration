a0e84c16a2265be5faa1f86afe6438a862a16d98
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-25 16:20:27 +0900

    :fire: Remove 이벤트 핸들러
    #12 이벤트 핸들러 삭제 후 다시 구현할 예정

--- src/components/Footer/Buttons.jsx
+++ src/components/Footer/Buttons.jsx
@@ -1,95 +1,55 @@
 import React from 'react';
 import * as S from './Footer.styled';
 import { ReactComponent as Home } from '../../assets/images/Home.svg';
-import { ReactComponent as Browse } from '../../assets/images/Home.svg';
-import { ReactComponent as Add } from '../../assets/images/Home.svg';
-import { ReactComponent as Chat } from '../../assets/images/Home.svg';
-import { ReactComponent as User } from '../../assets/images/Home.svg';
+import { ReactComponent as Browse } from '../../assets/images/Browse.svg';
+import { ReactComponent as Add } from '../../assets/images/Add.svg';
+import { ReactComponent as Chat } from '../../assets/images/Chat.svg';
+import { ReactComponent as User } from '../../assets/images/User.svg';
 
 const mainColor = '#45522b';
 const basicColor = '#0f0f0f';
 
-export const HomeBtn = ({ hover, handleHover, handleClick, clicked }) => {
+export const HomeBtn = ({}) => {
     return (
-        <S.IconButton
-            type="button"
-            name="home"
-            onMouseEnter={() => handleHover(true)}
-            onMouseLeave={() => handleHover(false)}
-            onClick={handleClick}
-        >
-            <Home stroke={mainColor} />
-            <S.IconName hover={hover.toString()} clicked={clicked.toString()}>
-                홈
-            </S.IconName>
+        <S.IconButton type="button" name="home">
+            <Home stroke={basicColor} />
+            <S.IconName>홈</S.IconName>
         </S.IconButton>
     );
 };
 
-export const BrowseBtn = ({ hover, handleHover, handleClick, clicked }) => {
+export const BrowseBtn = () => {
     return (
-        <S.IconButton
-            type="button"
-            name="browse"
-            onMouseEnter={() => handleHover(true)}
-            onMouseLeave={() => handleHover(false)}
-            onClick={handleClick}
-        >
-            <Browse stroke={mainColor} />
-            <S.IconName hover={hover.toString()} clicked={clicked.toString()}>
-                피드
-            </S.IconName>
+        <S.IconButton type="button" name="browse">
+            <Browse stroke={basicColor} />
+            <S.IconName>피드</S.IconName>
         </S.IconButton>
     );
 };
 
-export const AddBtn = ({ hover, handleHover, handleClick, clicked }) => {
+export const AddBtn = () => {
     return (
-        <S.IconButton
-            type="button"
-            name="add"
-            onMouseEnter={() => handleHover(true)}
-            onMouseLeave={() => handleHover(false)}
-            onClick={handleClick}
-        >
-            <Add stroke={mainColor} />
-            <S.IconName hover={hover.toString()} clicked={clicked.toString()}>
-                글쓰기
-            </S.IconName>
+        <S.IconButton type="button" name="add">
+            <Add stroke={basicColor} />
+            <S.IconName>글쓰기</S.IconName>
         </S.IconButton>
     );
 };
 
-export const ChatBtn = ({ hover, handleHover, handleClick, clicked }) => {
+export const ChatBtn = ({}) => {
     return (
-        <S.IconButton
-            type="button"
-            name="chat"
-            onMouseEnter={() => handleHover(true)}
-            onMouseLeave={() => handleHover(false)}
-            onClick={handleClick}
-        >
-            <Chat fill={mainColor} />
-            <S.IconName hover={hover.toString()} clicked={clicked.toString()}>
-                채팅
-            </S.IconName>
+        <S.IconButton type="button" name="chat">
+            <Chat fill={basicColor} />
+            <S.IconName>채팅</S.IconName>
         </S.IconButton>
     );
 };
 
-export const UserBtn = ({ hover, handleHover, handleClick, clicked }) => {
+export const UserBtn = ({}) => {
     return (
-        <S.IconButton
-            type="button"
-            name="user"
-            onMouseEnter={() => handleHover(true)}
-            onMouseLeave={() => handleHover(false)}
-            onClick={handleClick}
-        >
-            <User stroke={mainColor} />
-            <S.IconName hover={hover.toString()} clicked={clicked.toString()}>
-                프로필
-            </S.IconName>
+        <S.IconButton type="button" name="user">
+            <User stroke={basicColor} />
+            <S.IconName>프로필</S.IconName>
         </S.IconButton>
     );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,51 +10,14 @@ const Footer = () => {
         chat: false,
         user: false,
     });
-    const [clickedBtn, setClickedBtn] = useState('');
-
-    const handleHover = (btnName, state) => {
-        setHover(prev => ({
-            ...prev,
-            [btnName]: state,
-        }));
-    };
-
-    const handleClick = event => {
-        setClickedBtn(event.currentTarget.name);
-    };
 
     return (
         <S.Footer>
-            <HomeBtn
-                hover={hover.home}
-                handleHover={state => handleHover('home', state)}
-                handleClick={handleClick}
-                clicked={clickedBtn === 'home'}
-            />
-            <BrowseBtn
-                hover={hover.browse}
-                handleHover={state => handleHover('browse', state)}
-                handleClick={handleClick}
-                clicked={clickedBtn === 'browse'}
-            />
-            <AddBtn
-                hover={hover.add}
-                handleHover={state => handleHover('add', state)}
-                handleClick={handleClick}
-                clicked={clickedBtn === 'add'}
-            />
-            <ChatBtn
-                hover={hover.chat}
-                handleHover={state => handleHover('chat', state)}
-                handleClick={handleClick}
-                clicked={clickedBtn === 'chat'}
-            />
-            <UserBtn
-                hover={hover.user}
-                handleHover={state => handleHover('user', state)}
-                handleClick={handleClick}
-                clicked={clickedBtn === 'user'}
-            />
+            <HomeBtn />
+            <BrowseBtn />
+            <AddBtn />
+            <ChatBtn />
+            <UserBtn />
         </S.Footer>
     );
 };

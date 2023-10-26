bfcb8a8d8812862e780d12f891bbfff4c5ad9043
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-26 13:14:21 +0900

    :sparkles: Add 이미지 업로드 기능, 시작하기 버튼
    #23 프로필 이미지 업로드 기능, 시작하기 버튼 스타일 구현

--- src/pages/Profile/ProfileUpload.styled.jsx
+++ src/pages/Profile/ProfileUpload.styled.jsx
@@ -1,1 +1,48 @@
 import styled from 'styled-components';
+
+export const ProfileTitle = styled.h4`
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.fontSize.lg};
+`;
+
+export const ProfileMain = styled.div`
+    padding: 0 25px;
+`;
+export const ProfileImgContainer = styled.div`
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    margin: 0 auto;
+`;
+
+export const ProfileImg = styled.img`
+    width: 110px;
+    height: 110px;
+    border-radius: 50%;
+    border: 1px solid ${({ theme }) => theme.border};
+    object-fit: cover;
+`;
+
+export const ImgUploadInput = styled.input`
+    display: none;
+`;
+
+export const ImgUploadLabel = styled.label`
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 36px;
+    height: 36px;
+    padding: 4px;
+    background-color: ${({ theme }) => theme.point};
+    border-radius: 50%;
+    cursor: pointer;
+`;
+
+export const ImgUploadContainer = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+`;

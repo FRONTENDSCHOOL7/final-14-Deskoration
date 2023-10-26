18c912112d081d26bb83de09f1b5f42bd631ca2f
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2023-10-26 16:21:32 +0900

    :sparkles: Add 게시글 상세보기 헤더, 댓글 구현 중
    #6 게시글 상세보기 헤더 하드코딩 구현, 댓글 구현하는 중

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import * as S from './Board.styled';
+import GradientButton from '../../components/GradientButton/GradientButton';
 
 const Board = () => {
-    return <div>Board Junha, hyeonjin start!</div>;
+    return (
+        <>
+            <S.BoardHeader>
+                <S.BoardHeaderUser>
+                    <button>
+                        <S.BackIcon />
+                    </button>
+                    <S.ProfileImg></S.ProfileImg>
+                    <div>프로필ID</div>
+                </S.BoardHeaderUser>
+                <GradientButton gra={true} width={'80px'} padding={'5px 0'}>
+                    팔로우
+                </GradientButton>
+            </S.BoardHeader>
+            <S.CommentSection>
+                <S.CommentCounter>총 x개의 댓글</S.CommentCounter>
+                댓글창
+            </S.CommentSection>
+        </>
+    );
 };
 
 export default Board;
--- src/pages/Board/Board.styled.jsx
+++ src/pages/Board/Board.styled.jsx
@@ -1,1 +1,45 @@
 import styled from 'styled-components';
+import { ReactComponent as Back } from '../../assets/images/Backward.svg';
+import theme from '../../styles/theme';
+
+export const BoardHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+
+    align-items: center;
+    height: 70px;
+    margin: 0 25px;
+`;
+
+export const BoardHeaderUser = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
+
+export const BackIcon = styled(Back)`
+    vertical-align: top;
+`;
+
+export const ProfileImg = styled.div`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-image: url('/images/puppy.jpg');
+    background-size: cover;
+    background-position: center;
+    box-sizing: border-box;
+`;
+
+export const CommentSection = styled.section`
+    width: 100%;
+    padding: 20px 25px;
+    border-top: 1px solid #d9d9d9;
+    border-bottom: 1px solid #d9d9d9;
+`;
+
+export const CommentCounter = styled.div`
+    color: ${theme.subFont};
+`;
+
+export const Comment = styled.div``;

5d8d05640cf79c4d8ee29a78b316fce8ecc5438b
yen <l.yennie.j@gmail.com>
2023-10-26 22:42:08 +0900

    :sparkles: Add User markup
    유저 컴포넌트 기본 마크업 Fixes: #19

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import * as S from './User.styled';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const User = () => {
-    return <S.UserContainer>User</S.UserContainer>;
+    return (
+        <S.UserContainer>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="login">로그인</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="signup">회원가입</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+
+            <section>
+                <ul>
+                    <li>
+                        <button>구글</button>
+                    </li>
+                    <li>
+                        <button>카카오</button>
+                    </li>
+                    <li>
+                        <button>페북</button>
+                    </li>
+                </ul>
+            </section>
+        </S.UserContainer>
+    );
 };
 
 export default User;

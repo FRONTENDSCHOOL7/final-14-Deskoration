765e48b716d7707f2c006b86bde7c558e917d56f
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-26 23:08:26 +0900

    :label: Change warniing props 타입 변경
    Related to: #4,#19,#25 warning props 타입을 문자열에서 불리언으로 변경

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import * as S from './Input.styled';
 
 // label : input의 항목 이름
 // ref : onchange 이벤트로 입력한 텍스트를 저장한 ref 변수
 // fn : onChange 이벤트에 들어갈 함수
-// warning : 경고 메세지를 띄워야할 경우 문자열 'warning'을 props로 전달('warning'일 경우 밑줄이 빨간색)
+// warning : 경고 메세지를 띄워야할 경우 불리언 true를 props로 전달(true일 경우 밑줄이 빨간색)
 
 // Input 컴포넌트를 가져다 사용할 컴포넌트에서 아래 예제와 같이 사용하시면 됩니다. (해당 주석과 예제는 추후 삭제 예정)
 
-// const Example = () => {
+// export const Example = () => {
 //     const inputEl = useRef(null);
 //     const [data, setData] = useState('');
 //     const handleInput = event => {
@@ -18,11 +18,16 @@ import * as S from './Input.styled';
 
 //     const handleClick = () => {
 //         console.log(data);
-//     }
+//     };
 
 //     return (
 //         <>
-//             <Input label="카테고리" $ref={inputEl} fn={handleInput} warning='warning'/>
+//             <Input
+//                 label="카테고리"
+//                 $ref={inputEl}
+//                 fn={handleInput}
+//                 warning={false}
+//             />
 //             <button onClick={handleClick}>버튼</button>
 //         </>
 //     );
@@ -36,7 +41,7 @@ const Input = ({ label, $ref, fn, warning }) => {
                 id={label}
                 ref={$ref}
                 onChange={fn}
-                className={warning}
+                className={warning ? 'warning' : null}
             />
         </>
     );

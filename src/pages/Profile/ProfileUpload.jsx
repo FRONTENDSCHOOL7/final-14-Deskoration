51cf9847aa8653183651140fc0610c2779b8ff6c
Jungmin Moon <jm.moon0801@gmail.com>
2023-10-27 13:06:28 +0900

    :sparkles: Add 유효성 검사 기능
    #25 사용자 이름, id, 소개글 유효성 검사 기능 구현

--- src/pages/Profile/ProfileUpload.jsx
+++ src/pages/Profile/ProfileUpload.jsx
@@ -10,6 +10,7 @@ import GradientButton from '../../components/GradientButton/GradientButton';
 export const ProfileUpload = () => {
     const [photoURL, setPhotoURL] = useState(basicImg);
     const [data, setData] = useState({
+        image: '',
         userName: '',
         id: '',
         intro: '',
@@ -21,12 +22,17 @@ export const ProfileUpload = () => {
     const idEl = useRef(null);
     const introEl = useRef(null);
 
+    // 업로드한 이미지 url 저장
     const handleFileChange = event => {
         const file = event.target.files[0];
         if (!file) {
             setPhotoURL(prev => setPhotoURL(prev));
         } else {
             ImgConvert(file, setPhotoURL);
+            setData(prev => ({
+                ...prev,
+                image: photoURL,
+            }));
         }
     };
 
@@ -56,10 +62,33 @@ export const ProfileUpload = () => {
         }
     };
 
+    // 유효성 검사 함수
+    const checkValidUserName = userName => {
+        return userName.length >= 2 && userName.length <= 10;
+    };
+
+    const checkValidID = id => {
+        const regex = /^[a-zA-Z0-9._]+$/;
+        return id.length >= 2 && regex.test(id);
+    };
+
+    const checkValidIntro = intro => {
+        return intro.length > 0;
+    };
+
+    // submit 함수
     const dataSubmit = event => {
         event.preventDefault();
         setInitialAccess(false);
-        console.log('data');
+        if (
+            !checkValidUserName(data.userName) ||
+            !checkValidID(data.id) ||
+            !checkValidIntro(data.intro)
+        ) {
+            return; // 유효성 검사 실패
+        }
+
+        console.log(data);
     };
 
     return (
@@ -91,44 +120,38 @@ export const ProfileUpload = () => {
                             $ref={userNameEl}
                             fn={event => handleInput(event, '사용자 이름')}
                             warning={
-                                !initialAccess && !data.userName ? true : false
+                                !initialAccess &&
+                                !checkValidUserName(data.userName)
                             }
                         />
-                        {initialAccess
-                            ? null
-                            : !data.userName && (
-                                  <WarningMsg msg={'2~10자 이내여야 합니다.'} />
-                              )}
+                        {!initialAccess &&
+                            !checkValidUserName(data.userName) && (
+                                <WarningMsg msg={'2~10자 이내여야 합니다.'} />
+                            )}
                         <Input
                             label="계정 ID"
                             $ref={idEl}
                             fn={event => handleInput(event, '계정 ID')}
-                            warning={!initialAccess && !data.id ? true : false}
+                            warning={!initialAccess && !checkValidID(data.id)}
                         />
-                        {initialAccess
-                            ? null
-                            : !data.id && (
-                                  <WarningMsg
-                                      msg={
-                                          '영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
-                                      }
-                                  />
-                              )}
+                        {!initialAccess && !checkValidID(data.id) && (
+                            <WarningMsg
+                                msg={
+                                    '2글자 이상이며, 영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
+                                }
+                            />
+                        )}
                         <Input
                             label="소개"
                             $ref={introEl}
                             fn={event => handleInput(event, '소개')}
                             warning={
-                                !initialAccess && !data.intro ? true : false
+                                !initialAccess && !checkValidIntro(data.intro)
                             }
                         />
-                        {initialAccess
-                            ? null
-                            : !data.intro && (
-                                  <WarningMsg
-                                      msg={'자신에 대해서 소개해주세요!'}
-                                  />
-                              )}
+                        {!initialAccess && !checkValidIntro(data.intro) && (
+                            <WarningMsg msg={'자신에 대해서 소개해주세요!'} />
+                        )}
                     </S.InputBox>
                     <GradientButton
                         children="시작하기"

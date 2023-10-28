d723cb77700bd4b6e7c9b49fb3c51939b9d0872a
HyeonJin <ocn4091@naver.com>
2023-10-28 17:21:37 +0900

    :ok_hand: Add 뒤로 이동하기 기능 추가, 채팅방 이동시 오류 해결

--- src/pages/Chat/ChatRoomPage/ChatRoomPage.jsx
+++ src/pages/Chat/ChatRoomPage/ChatRoomPage.jsx
@@ -1,40 +1,117 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as S from './ChatRoomPage.styled';
-import { useParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ReactComponent as Backward } from '../../../assets/images/Backward.svg';
+import { ReactComponent as Send } from '../../../assets/images/Send.svg';
+import { ReactComponent as Clip } from '../../../assets/images/Clip.svg';
 
 const ChatRoomPage = () => {
     const location = useLocation();
+    const chatContainerRef = useRef(null); // Ref를 생성하여 채팅 컨테이너에 접근
+    const navigate = useNavigate();
 
-    // props를 받습니다.
-    const { username } = useParams();
+    const newData = new Date();
+    let hours = newData.getHours();
+    const minutes = newData.getMinutes().toString().padStart(2, '0');
 
-    // ChatListPage.js에서 전달한 데이터를 가져옵니다.
-    const { user, lastMessage, image /* 다른 데이터 필드 */ } = location.state;
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+
+    if (hours > 12) {
+        hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+
+    const formattedTime = `${hours}:${minutes} ${amPm}`;
+
+    const { user, message, image } = location.state;
+
+    const [newMessage, setNewMessage] = useState(''); // 문자열로 초기화
+
+    const [chatMessages, setChatMessages] = useState([]);
+
+    const handleGoBack = () => {
+        navigate(-1); // Use navigate to go back to the previous page
+    };
+
+    const handleSendMessage = () => {
+        if (newMessage.trim() === '') {
+            return; // 빈 메시지 전송 방지
+        }
+
+        // 새 메시지를 배열에 추가
+        setChatMessages(prevMessages => [
+            ...prevMessages,
+            { text: newMessage, isSentByUser: true },
+        ]);
+
+        // 메시지 입력 필드 초기화
+        setNewMessage('');
+    };
+
+    // useEffect를 사용하여 스크롤을 항상 맨 아래로 이동
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop =
+                chatContainerRef.current.scrollHeight;
+        }
+    }, [chatMessages]);
+
 
     return (
         <>
             <S.ChatRoomPageContainer>
-                <S.ChatListHeader>
-                    {/* header button 가져오기 */}
-                    <button>
-                        <img src="/images/icon-backward.jpg" alt="" />
+                <S.ChatRoomHeader>
+                    <button onClick={handleGoBack}>
+                        <Backward />
                     </button>
-                    <h2>
-                        <img src={image} alt="" className="user-img" />
-                        {username}
-                    </h2>
-                </S.ChatListHeader>
+                    <img src={image} alt="" className="user-img" />
+                    <h2>{user}</h2>
+                </S.ChatRoomHeader>
+                <S.ChatRoomMain ref={chatContainerRef}>
+                    {/* 상대 채팅 */}
+                    {message[0].map((receivedMessage, index) => (
+                        <S.ChatContent key={index} issentbyuser="false">
+                            <img src={image} alt="" className="userChat-img" />
+                            <div>
+                                <S.ChatBubble issentbyuser="false">
+                                    <p>{receivedMessage}</p>
+                                </S.ChatBubble>
+                                <p>{formattedTime}</p>
+                            </div>
+                        </S.ChatContent>
+                    ))}
+
+                    {/* 나의 채팅 */}
+                    {chatMessages.map((chat, index) => (
+                        <S.ChatContent key={index} issentbyuser="true">
+                            <div>
+                                <S.ChatBubble issentbyuser="true">
+                                    <p>{chat.text}</p>
+                                </S.ChatBubble>
+                                <p className="my-time">{formattedTime}</p>
+                            </div>
+                        </S.ChatContent>
+                    ))}
+                </S.ChatRoomMain>
             </S.ChatRoomPageContainer>
+            <S.ChatRoomPageFooter>
+                <S.ChatInput>
+                    <Clip />
+                    <input
+                        type="text"
+                        value={newMessage}
+                        onChange={e => setNewMessage(e.target.value)}
+                        className="chat-input"
+                        placeholder="메세지를 입력하세요"
+                    />
+                    <button onClick={handleSendMessage}>
+                        <Send className="chat-send" />
+                    </button>
+                </S.ChatInput>
+            </S.ChatRoomPageFooter>
         </>
     );
 };
 
 export default ChatRoomPage;
-
-// <h2>Chat Room for {username}</h2>
-//             <div>
-//                 <p>Username: {user}</p>
-//                 <p>Last Message: {lastMessage}</p>
-//                 {/* 다른 데이터 필드 표시 */}
-//             </div>
--- src/pages/Chat/ChatRoomPage/ChatRoomPage.styled.jsx
+++ src/pages/Chat/ChatRoomPage/ChatRoomPage.styled.jsx
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const ChatRoomPageContainer = styled.div`
     position: relative;
-    margin: 0 25px;
 `;
-export const ChatListHeader = styled.div`
+export const ChatRoomHeader = styled.div`
     display: flex;
     width: 310px;
     height: 70px;
     padding-top: 20px;
+    margin: 0 25px;
+
     font-size: 24px;
     align-items: center;
 
@@ -18,5 +19,79 @@ export const ChatListHeader = styled.div`
         border-radius: 100%;
         margin-left: 8px;
         margin-right: 8px;
+    }
+`;
+
+export const ChatRoomMain = styled.div`
+    width: 100%;
+    height: 640px;
+    padding-top: 20px;
+    overflow-y: scroll; /* 세로 스크롤을 활성화합니다. */
+
+    .userChat-img {
+        width: 36px;
+        height: 36px;
+        border-radius: 100%;
+        margin-left: 8px;
+        margin-right: 8px;
+    }
+`;
+
+export const ChatContent = styled.div`
+    display: flex;
+    margin: 0 25px 10px;
+    justify-content: ${props =>
+        props.issentbyuser === 'true' ? 'flex-end' : 'flex-start'}; // 문자열 비교
+    margin-top: 10px;
+    .my-time {
+        display: flex;
+        justify-content: ${props =>
+            props.issentbyuser === 'true' ? 'flex-end' : 'flex-start'}; // 문자열 비교
+    }
+`;
+
+export const ChatBubble = styled.div`
+    max-width: 220px;
+    background-color: ${props =>
+        props.issentbyuser === 'true' ? '#ECEBEB' : '#FFFFFF'}; // 문자열 비교
+    border: ${props =>
+        props.issentbyuser === 'true' ? '#ECEBEB' : '1px solid gray'}; // 문자열 비교
+    border-radius: ${props =>
+        props.issentbyuser === 'true'
+            ? '15px 0 15px 15px'
+            : '0 15px 15px 15px'}; // 문자열 비교
+    color: black; /* 말풍선 텍스트 색상 */
+    padding: 10px 20px; /* 말풍선 안의 여백 */
+    margin: 5px 0; /* 말풍선 간격 설정 */
+`;
+
+
+export const ChatRoomPageFooter = styled.div`
+    width: 360px;
+    height: 90px;
+    border-top: 1px solid black;
+`;
+
+export const ChatInput = styled.div`
+    width: 360px;
+    height: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 25px 25px 25px 25px;
+
+    .chat-input {
+        width: 225px;
+        height: 40px;
+        border: none;
+        outline: none;
+        background-color: #dbdbdb;
+        border-radius: 20px;
+        padding-left: 20px;
+    }
+
+    .chat-send {
+        width: 30px;
+        height: 30px;
     }
 `;

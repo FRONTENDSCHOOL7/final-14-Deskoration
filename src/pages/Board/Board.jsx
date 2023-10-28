c72142918bb9043ff12f10ccbc12cc5f17abeaca
HyeonJin <ocn4091@naver.com>
2023-10-28 14:30:08 +0900

    :lipstick: Add 게시글 상세보기 메인부분과 하단 댓글입력 푸터 UI 구현

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import * as S from './Board.styled';
 import GradientButton from '../../components/GradientButton/GradientButton';
+import { ReactComponent as Like } from '../../assets/images/Like.svg';
+import { ReactComponent as Comment } from '../../assets/images/Comment.svg';
+import { ReactComponent as Dots_vertical } from '../../assets/images/Dots_vertical.svg';
 
 const Board = () => {
     return (
@@ -17,17 +20,46 @@ const Board = () => {
                     팔로우
                 </GradientButton>
             </S.BoardHeader>
-            <S.CommentSection>
-                <S.CommentCounter>총 x개의 댓글</S.CommentCounter>
-                <S.AComment>
-                    <S.ProfileImg />
-                    <div>
-                        <S.CommentID>팔로우ID</S.CommentID>
-                        <S.Comment>대애애애애앳그으으으을</S.Comment>
+            <S.BoardMain>
+                <S.ContentSection>
+                    <img src="/images/puppy.jpg" alt="" />
+                    <img src="/images/puppy.jpg" alt="" />
+                    <img src="/images/puppy.jpg" alt="" />
+                    <div className="board-btn">
+                        <div className="btn-hc">
+                            <div>
+                                <Like className="like" />
+                            </div>
+                            <div>
+                                <Comment />
+                            </div>
+                        </div>
+                        <div>
+                            <Dots_vertical />
+                        </div>
                     </div>
-                    <S.DotsIcon />
-                </S.AComment>
-            </S.CommentSection>
+                    <h2 className="user-name">프로필ID</h2>
+                    <p className="main-content">
+                        우리집 강아지예요 정말 귀엽죠? 이름은 해피입니다!!
+                        해피라는 이름은 저희 집안에 행복을 가져다 주는 존재인것
+                        같아 이름을 해피로 지었어요 ㅎㅎ
+                    </p>
+                    {/* <S.CommentCounter>총 x개의 댓글</S.CommentCounter> */}
+                </S.ContentSection>
+                <S.CommentSection>댓글창</S.CommentSection>
+            </S.BoardMain>
+            <S.BoardFooter>
+                <S.CommentInput>
+                    <input
+                        type="text"
+                        placeholder="메시지를 입력하세요"
+                        className="input-text"
+                    />
+                    <button className="comment-btn">
+                        <p>등록</p>
+                    </button>
+                </S.CommentInput>
+            </S.BoardFooter>
         </>
     );
 };

c790d06f91db883e02c68079aa94d1f8f6b1333a
yen <l.yennie.j@gmail.com>
2023-10-29 04:38:32 +0900

    :sparkles: Add Signup page
    회원가입 페이지 유효성검사, 경고 메세지, useNavigate 사용으로 ProfileUpload 컴포넌트로 보낼 준비 완료 Fixes:#19 Related to:#23

--- src/pages/User/Signup/Signup.jsx
+++ src/pages/User/Signup/Signup.jsx
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Input from '../../../components/Input/Input';
+import { WarningMsg } from '../../../components/Input/WarningMsg';
+import { GradientButton } from '../../../components/GradientButton/GradientButton.styled';
+
 import * as S from './Signup.styled';
 
 const Signup = () => {
-    return <S.SignupForm>회원가입</S.SignupForm>;
+    const navigate = useNavigate();
+
+    const [warmEmail, setWarnEmail] = useState(true);
+    const [warnPassword, setWarnPassword] = useState(true);
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const onSubmit = event => {
+        event.preventDefault();
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        function checkPasswordFormat(password) {
+            return password.length >= 6;
+        }
+        const emailValue = emailRef.current.value;
+        const passwordValue = passwordRef.current.value;
+
+        const validEmail = emailRegex.test(emailValue);
+        const validPassword = checkPasswordFormat(passwordValue);
+
+        if (validEmail && validPassword) {
+            console.log('오케이');
+            // navigate('/profileUpload', {
+            //     state: { emailValue: emailValue, passwordValue: passwordValue },
+            // });
+        } else {
+            !validEmail ? setWarnEmail(false) : setWarnEmail(true);
+            !validPassword ? setWarnPassword(false) : setWarnPassword(true);
+        }
+    };
+
+    return (
+        <S.SignupForm onSubmit={onSubmit}>
+            <S.InputBox>
+                <Input
+                    label={'email'}
+                    inputRef={emailRef}
+                    warning={!warmEmail}
+                />
+                {!warmEmail && (
+                    <WarningMsg msg={'이메일 형식이 올바르지 않습니다.'} />
+                )}
+                <Input
+                    label={'password'}
+                    inputRef={passwordRef}
+                    warning={!warnPassword}
+                />
+                {!warnPassword && (
+                    <WarningMsg msg={'비밀번호는 6자 이상이여야 합니다.'} />
+                )}
+            </S.InputBox>
+            <GradientButton $gra={true} width={'100%'} $padding={'20px'}>
+                회원가입
+            </GradientButton>
+        </S.SignupForm>
+    );
 };
 
 export default Signup;

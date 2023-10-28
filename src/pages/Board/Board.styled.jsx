ee5f5288f833b253b53bb1efd1eb711d6b17cf8d
HyeonJin <ocn4091@naver.com>
2023-10-28 14:30:55 +0900

    :lipstick: Add 게시글 상세보기 메인, 푸터 스타일 구현

--- src/pages/Board/Board.styled.jsx
+++ src/pages/Board/Board.styled.jsx
@@ -34,9 +34,58 @@ export const ProfileImg = styled.div`
     box-sizing: border-box;
 `;
 
+export const BoardMain = styled.div`
+    width: 100%;
+    height: 640px;
+    padding-left: 20px;
+    padding-right: 20px;
+    overflow: scroll; /* 스크롤 생성을 위해 수정 */
+`;
+
+export const ContentSection = styled.div`
+    max-width: 100%;
+    max-height: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    img {
+        width: 100%;
+        height: auto;
+        margin: 5px 0 5px 0;
+    }
+
+    .board-btn {
+        display: flex;
+        justify-content: space-between;
+        margin: 5px 0 5px 0;
+    }
+
+    .btn-hc {
+        display: flex;
+    }
+
+    .like {
+        margin-right: 10px;
+    }
+
+    .user-name {
+        font-size: 14px;
+        font-weight: bold;
+        margin: 5px 0 5px 0;
+    }
+
+    .main-content {
+        margin: 5px 0 5px 0;
+    }
+`;
+
 export const CommentSection = styled.section`
     width: 100%;
-    padding: 20px 25px;
+    height: auto;
+    padding: 20px 0 25px 10px;
+    margin-top: 10px;
     border-top: 1px solid #d9d9d9;
     border-bottom: 1px solid #d9d9d9;
 `;
@@ -46,22 +95,35 @@ export const CommentCounter = styled.div`
     margin-bottom: 20px;
 `;
 
-export const AComment = styled.div`
+export const BoardFooter = styled.div`
+    width: 100%;
+    height: 90px;
+    padding: 15px 25px 15px 25px;
+`;
+
+export const CommentInput = styled.div`
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
     display: flex;
-    align-items: center;
-    gap: 10px;
-`;
+    justify-content: space-between;
+    padding: 15px;
 
-export const CommentID = styled.p`
-    font-family: 'PreBold';
-    font-size: 12px;
-    margin-bottom: 5px;
-`;
+    .input-text {
+        width: 100%;
+        margin-right: 10px;
+        height: 100%;
+        outline: none;
+        border: none;
+        background: none;
+        font-size: 14px;
+    }
 
-export const Comment = styled.p``;
-
-export const DotsIcon = styled(Dots)`
-    position: relative;
-    left: 20%;
-    transform: rotate(90deg);
+    .comment-btn {
+        color: orange;
+        width: 30px;
+        height: 30px;
+    }
 `;

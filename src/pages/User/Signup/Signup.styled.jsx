00fde8bde518b6c6bac7de45226ddbdbc716aa38
yen <l.yennie.j@gmail.com>
2023-10-29 04:39:26 +0900

    :lipstick: Add Signup UI
    회원가입 기본 UI설정 Fixes:#19

--- src/pages/User/Signup/Signup.styled.jsx
+++ src/pages/User/Signup/Signup.styled.jsx
@@ -1,3 +1,9 @@
 import { styled } from 'styled-components';
 
-export const SignupForm = styled.form``;
+export const SignupForm = styled.form`
+    padding: 20px 0;
+`;
+
+export const InputBox = styled.div`
+    margin-bottom: 20px;
+`;

525395c353dcf06928ed8afd41b9fea312a6ad58
yen <l.yennie.j@gmail.com>
2023-10-28 21:21:44 +0900

    :bug: Add button type
    기본 버튼 타입은 'button'으로 prop전달 하지 않아도 됌. submit일 경우에만 typ={'submit'}으로 전달 Resolves: #33

--- src/components/GradientButton/GradientButton.jsx
+++ src/components/GradientButton/GradientButton.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import * as S from './GradientButton.styled';
 
-const GradientButton = ({ children, gra, width, padding, onClick }) => {
+const GradientButton = ({ children, type, gra, width, padding, onClick }) => {
     return (
         <S.GradientButton
-            type="button"
+            type={type}
             $gra={gra}
             width={width}
             $padding={padding}

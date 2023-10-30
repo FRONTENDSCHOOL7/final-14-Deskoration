48abb1736069d076888c4d32c9fbfa6d66f7851b
Herrypi <ocn4091@naver.com>
2023-10-30 13:10:36 +0900

    :lipstick: Add 프로필 페이지 UI 구현
    #38 프로필 페이지 마크업 구현

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Profile.styled';
+import Footer from '../../components/Footer/Footer';
+import GradientButton from '../../components/GradientButton/GradientButton';
+import Article from '../Home/Article';
 
 const Profile = () => {
-    return <div>Profile</div>;
+    const content =
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum minima possimus praesentium'; // 여기에 표시할 텍스트 추가
+    return (
+        <>
+            <S.ProfileHeader>
+                <button>
+                    <S.Backwardicon />
+                </button>
+                <h2>user</h2>
+            </S.ProfileHeader>
+            <S.ProfileContainer>
+                <S.UserInfo>
+                    <img
+                        src="/images/DeskSetup.jpg"
+                        alt=""
+                        className="user-img"
+                    />
+                    <div className="user-introduce">
+                        <p className="user-name">username</p>
+                        <p className="user-info">
+                            {content}
+                            <button>더보기</button>
+                        </p>
+                    </div>
+                </S.UserInfo>
+                <GradientButton
+                    type={'button'}
+                    gra={'true'}
+                    width={'310px'}
+                    padding={'10px'}
+                >
+                    프로필 편집
+                </GradientButton>
+                <S.UserDataList>
+                    <button className="user-post">
+                        <p>1234</p>
+                        <p>게시물</p>
+                    </button>
+                    <button className="user-follow">
+                        <p>1234</p>
+                        <p>팔로우</p>
+                    </button>
+                    <button className="user-following">
+                        <p>1234</p>
+                        <p>팔로잉</p>
+                    </button>
+                </S.UserDataList>
+                <S.UserPostings>
+                    <Article />
+                </S.UserPostings>
+            </S.ProfileContainer>
+            <Footer />
+        </>
+    );
 };
 
 export default Profile;

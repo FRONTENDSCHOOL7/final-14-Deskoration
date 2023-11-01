c282399fcf14b501a5d1f1a6c8bc4b76ccf5c6bf
yen <l.yennie.j@gmail.com>
2023-11-01 23:23:56 +0900

    :seedling: Add Router.jsx
    #49 라우터 관리 하기 위한 기본 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
-import Home from './pages/Home/Home';
-
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // 추후 라우터 설정하면 라우터에 css적용할 예정입니다.
 import './App.css';
-import User from './pages/User/User';
-import Login from './pages/User/Login';
-import Signup from './pages/User/Signup';
-import NewBoard from './pages/NewBoard/NewBoard';
-
-const token = sessionStorage.getItem('tempToken');
+import Router from './router/Router';
 
 function App() {
     return (
         <div className="app-body">
-            <div className="container">
+            <div className="container" style={{ padding: '0 25px' }}>
                 <ThemeProvider theme={theme}>
                     <GlobalStyle />
-                    <BrowserRouter>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={<Navigate to="/login" replace />}
-                            />
-                            {token ? (
-                                <>
-                                    <Route
-                                        path={'/home'}
-                                        element={<Home />}
-                                    ></Route>
-                                    <Route
-                                        path={'/newboard'}
-                                        element={<NewBoard />}
-                                    ></Route>
-                                    <Route
-                                        path="/newboard/:id"
-                                        element={<NewBoard />}
-                                    />
-                                </>
-                            ) : (
-                                <Route path="/" element={<User />}>
-                                    <Route path="/login" element={<Login />} />
-                                    <Route
-                                        path="/signup"
-                                        element={<Signup />}
-                                    />
-                                </Route>
-                            )}
-                        </Routes>
-                    </BrowserRouter>
+                    <Router />
                 </ThemeProvider>
             </div>
         </div>

afbb07f3f3154fd421d28801d95aee479a2e2e03
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-01 09:16:00 +0900

    :construction: Add 로딩화면 애니메이션 구현중
    #44 로딩화면 애니메이션 구현중

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './Loading.styled';
 
-const Loading = () => {
-    return <div>Loading</div>;
+const Loading = ({ text }) => {
+    const [toggleColor, setToggleColor] = useState(false);
+
+    useEffect(() => {
+        const textAnimationLoop = () => {
+            setToggleColor(true);
+
+            setTimeout(() => {
+                setToggleColor(false);
+            }, 2500);
+        };
+
+        const intervalId = setInterval(textAnimationLoop, 5000);
+
+        // 초기 애니메이션
+        textAnimationLoop();
+
+        // 컴포넌트가 언마운트 될 때 인터벌 클리어
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <S.LogoContainer>
+            <S.LoadingImg />
+            <S.TitleBox>
+                {text.split('').map((char, index) => (
+                    <S.CharSpan
+                        key={index}
+                        $toggleColor={toggleColor}
+                        $delay={0.1 * index}
+                    >
+                        {char}
+                    </S.CharSpan>
+                ))}
+            </S.TitleBox>
+        </S.LogoContainer>
+    );
 };
 
 export default Loading;
--- src/pages/Loading/Loading.styled.jsx
+++ src/pages/Loading/Loading.styled.jsx
@@ -1,1 +1,42 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { ReactComponent as Logo } from '../../assets/images/Loading.svg';
+
+export const LogoContainer = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const LoadingImg = styled(Logo)`
+    aspect-ratio: 94 / 124;
+    height: 180px;
+
+    .custom-fill {
+        fill: none;
+    }
+`;
+
+export const TitleBox = styled.h1`
+    font-size: 24px;
+    font-weight: 700;
+    margin-top: 10px;
+`;
+
+export const CharSpan = styled.span`
+    ${props =>
+        props.$toggleColor
+            ? css`
+                  color: #fff;
+              `
+            : css`
+                  color: ${props => props.theme.logo};
+              `}
+    transition: color 1.5s;
+    ${props =>
+        props.$delay &&
+        css`
+            transition-delay: ${props.$delay}s;
+        `}
+`;

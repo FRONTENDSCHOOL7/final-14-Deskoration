fbec88615cc6cff7b3de1d33e8242f554609a438
yen <l.yennie.j@gmail.com>
2023-11-01 22:06:11 +0900

    :truck: Change router
    #41 temp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import Home from './pages/Home/Home';
-import Board from './pages/Board/Board';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // 추후 라우터 설정하면 라우터에 css적용할 예정입니다.
 import './App.css';
+import User from './pages/User/User';
+import Login from './pages/User/Login';
+import Signup from './pages/User/Signup';
+import NewBoard from './pages/NewBoard/NewBoard';
+
+const token = sessionStorage.getItem('tempToken');
 function App() {
     return (
         <div className="app-body">
@@ -15,8 +20,34 @@ function App() {
                     <GlobalStyle />
                     <BrowserRouter>
                         <Routes>
-                            <Route path={'/home'} element={<Home />}></Route>
-                            <Route path={'/board'} element={<Board />}></Route>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/login" replace />}
+                            />
+                            {token ? (
+                                <>
+                                    <Route
+                                        path={'/home'}
+                                        element={<Home />}
+                                    ></Route>
+                                    <Route
+                                        path={'/newboard'}
+                                        element={<NewBoard />}
+                                    ></Route>
+                                    <Route
+                                        path="/newboard/:id"
+                                        element={<NewBoard />}
+                                    />
+                                </>
+                            ) : (
+                                <Route path="/" element={<User />}>
+                                    <Route path="/login" element={<Login />} />
+                                    <Route
+                                        path="/signup"
+                                        element={<Signup />}
+                                    />
+                                </Route>
+                            )}
                         </Routes>
                     </BrowserRouter>
                 </ThemeProvider>

6b47a8384d8105b5761d0da2665a33c95b5fba25
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-04 11:29:45 +0900

    :truck: Move layout folder
    #75 레이아웃 폴더를 src 하위 폴더로 위치 변경

--- src/Layout/defaultLayout.jsx
+++ src/Layout/defaultLayout.jsx
@@ -1,5 +1,5 @@
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 const DefaultLayout = () => (
     <>

e669acc9c38718fa7a6b68828e854827e8881fb7
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-04 11:29:45 +0900

    :truck: Move layout folder
    #75 레이아웃 폴더를 src 하위 폴더로 위치 변경

--- src/Layout/NoHeaderLayout.jsx
+++ src/Layout/NoHeaderLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '../../components/Footer/Footer';
+import Footer from '../components/Footer/Footer';
 
 const NoHeaderLayout = () => (
     <>

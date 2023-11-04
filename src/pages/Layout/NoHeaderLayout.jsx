d9d0ac303394983b940dea126193329c1a612efb
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-04 11:05:54 +0900

    :sparkles: Add layout component
    #75 헤더유무에 따른 레이아웃 컴포넌트 생성

---
+++ src/pages/Layout/NoHeaderLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+
+const NoHeaderLayout = () => (
+    <>
+        <main>
+            <Outlet />
+        </main>
+        <Footer />
+    </>
+);
+
+export default NoHeaderLayout;
---
+++ src/pages/Layout/defaultLayout.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
+const DefaultLayout = () => (
+    <>
+        <Header />
+        <main style={{ padding: '0 25px' }}>
+            <Outlet />
+        </main>
+        <Footer />
+    </>
+);
+
+export default DefaultLayout;

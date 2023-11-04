6c14cc4e97c3f7610c8168dca89de8d1480d69e3
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-04 20:18:39 +0900

    :fire: Remove 불필요한 코드 삭제
    #75 불필한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
-import Home from './pages/Home/Home';
-import DetailPost from './pages/DetailPost/DetailPost';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
 // 추후 라우터 설정하면 라우터에 css적용할 예정입니다.
 import './App.css';
-import User from './pages/User/User';
-import Login from './pages/User/Login';
-import Signup from './pages/User/Signup';
 import Router from './router/Router';
 
 const token = sessionStorage.getItem('tempToken');
@@ -18,36 +11,10 @@ const token = sessionStorage.getItem('tempToken');
 function App() {
     return (
         <div className="app-body">
-            <div className="container" style={{ padding: '0 25px' }}>
+            <div className="container">
                 <ThemeProvider theme={theme}>
                     <GlobalStyle />
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path={'/home'} element={<Home />}></Route>
-                            <Route
-                                path={'/board'}
-                                element={<DetailPost />}
-                            ></Route>
-                            <Route
-                                path="/"
-                                element={<Navigate to="/login" replace />}
-                            />
-                            {token ? (
-                                <Route
-                                    path={'/home'}
-                                    element={<Home />}
-                                ></Route>
-                            ) : (
-                                <Route path="/" element={<User />}>
-                                    <Route path="/login" element={<Login />} />
-                                    <Route
-                                        path="/signup"
-                                        element={<Signup />}
-                                    />
-                                </Route>
-                            )}
-                        </Routes>
-                    </BrowserRouter>
+                    <Router />
                 </ThemeProvider>
             </div>
         </div>

0c8077774db659f379f4b59434207ce7508e17a5
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-05 21:46:51 +0900

    :lipstick: Change 헤더 뒤로 가기 버튼
    #75 푸터에 해당하는 페이지는 헤더 뒤로 가기 버튼을 숨김

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import * as S from './Header.styled';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleGoBack = () => {
         navigate(-1);
     };
     const currentPage = useSelector(state => state.pageTitle.currentPage);
 
+    const isHome = location.pathname.includes('/home');
+    const isFeed = location.pathname.includes('/feed');
+    const isNewBoard = location.pathname.includes('/newboard');
+    const isChat = location.pathname.includes('/chat');
+    const isMyProfile = location.pathname.includes('/profile');
+    const isUserProfile = location.pathname.includes('/userProfile');
+
     return (
         <>
             <S.Headbar>
-                <button type="button" onClick={handleGoBack}>
-                    <S.BackwardIcon />
-                </button>
+                {isHome ||
+                isFeed ||
+                isNewBoard ||
+                isChat ||
+                isMyProfile ||
+                isUserProfile ? null : (
+                    <button type="button" onClick={handleGoBack}>
+                        <S.BackwardIcon />
+                    </button>
+                )}
+
                 <div>
                     {currentPage.type === 'text' && (
                         <S.titleSpan>{currentPage.value}</S.titleSpan>

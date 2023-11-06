c44e1120656a44217b65cee59deff57c4eb2d47d
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2023-11-06 21:25:28 +0900

    :art: article 코드 정리
    article 파일 코드 정리

--- src/pages/Home/Article.jsx
+++ src/pages/Home/Article.jsx
@@ -1,25 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './Article.styled';
-import DetailPost from '../DetailPost/DetailPost';
-import { GetAllPost, GetMyPost } from '../../service/post_service';
+import { GetAllPost } from '../../service/post_service';
 
 const Article = () => {
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(false);
-    const sectionRef = useRef(null); // S.Section에 대한 참조 생성
+    const sectionRef = useRef(null);
     const token = sessionStorage.getItem('tempToken');
-    const tempAccountName = sessionStorage.getItem('tempAccountName');
 
-    const tempApi = async token => {
+    const fetchAllPost = async token => {
         try {
             const result = await GetAllPost(token);
-            // console.log('API보기2: ', result.posts);
             const deskoration = result.posts.filter(post =>
                 post.content?.includes('"deskoration"'),
             );
             console.log('deskoration: ', deskoration);
-            setArticles(deskoration); // Set the state with articles containing images
+            setArticles(deskoration);
         } catch (error) {
             console.error('error');
         }
@@ -27,10 +24,8 @@ const Article = () => {
 
     console.log('articles', articles);
     useEffect(() => {
-        const desk_result = tempApi(token);
+        fetchAllPost(token);
     }, []);
-
-    const postImage = articles.map(item => item.image);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -53,10 +48,9 @@ const Article = () => {
             setArticles(prevArticles => [
                 ...prevArticles,
                 ...Array(8).fill({}),
-            ]); // 예시로 8개의 게시물을 추가
+            ]);
             setLoading(false);
-        }, 1000); // 1초 지연으로 예시
-        console.log('스크롤 후 aricles:', articles);
+        }, 1000);
     };
 
     return (

3edcdaa5e0e98b1ff542ae13bd3942fac51dc338
yen <l.yennie.j@gmail.com>
2023-11-06 11:51:02 +0900

    :lipstick: Add position left
    position left:0 으로 화면 맞추기 Fixes: #88

--- src/components/BottomSheet/BottomSheet.styled.jsx
+++ src/components/BottomSheet/BottomSheet.styled.jsx
@@ -3,6 +3,7 @@ import styled, { keyframes, css } from 'styled-components';
 export const BottomSheetContainer = styled.div`
     position: absolute;
     top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     background: rgba(0, 0, 0, 0.1);
@@ -49,8 +50,6 @@ export const BottomSheetBox = styled.div`
     align-items: center;
     justify-content: center;
     gap: 15px;
-
-    z-index: 10000;
     ${props =>
         props.$isBottomSheet
             ? css`

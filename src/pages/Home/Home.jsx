33bd644f826abde4a3ebdfa27163e1463ed98be5
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-06 20:46:09 +0900

    :construction: Add 초기화면 로딩을 위한 기능 구현중
    splash 화면 로딩을 위한 기능 구현중

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './Home.styled';
 import logoImg from '../../assets/images/Logo.svg';
 import Article from './Article';
 import Slide from './Slide';
+import FirstConnectLoading from '../Loading/FirstConnectLoading';
 import usePageHandler from '../../hooks/usePageHandler';
 
 const Home = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
     usePageHandler('image', logoImg);
     return (
         <>
-            <Slide />
-            <Article />
+            {isLoading ? (
+                <FirstConnectLoading />
+            ) : (
+                <>
+                    <Slide />
+                    <Article />
+                </>
+            )}
         </>
     );
 };

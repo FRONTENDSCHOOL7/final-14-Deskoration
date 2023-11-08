335f8c58312ca9ce3d9b7f169cc0504de7c05540
yen <l.yennie.j@gmail.com>
2023-11-08 12:22:56 +0900

    :sparkles: Add alert modal
    확인을 위한 alert modal 생성 Fixes: #126

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import confirmModalReducer from './features/modal/confirmModalSlice';
+import alertModalReducer from './features/modal/alertModalSlice';
 import pageTitleReducer from './features/pageTitle/pageTitleSlice';
 
 export default configureStore({
     reducer: {
         confirmModal: confirmModalReducer,
+        alertModal: alertModalReducer,
         pageTitle: pageTitleReducer,
     },
 });

0ec5cb582c8be74c8e24d9dcd50e2ef4beab8f6b
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-27 14:31:12 +0900

    :lipstick: Change animation
    #92 keyframes를 사용하여 애니메이션 적용

--- src/components/Loading/Loader.styled.jsx
+++ src/components/Loading/Loader.styled.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 export const TitleBox = styled.h1`
     font-size: 24px;
@@ -9,19 +9,26 @@ export const TitleBox = styled.h1`
     align-items: center;
 `;
 
+const fadeIn = keyframes`
+  0% {
+    color: ${props => props.theme.main};
+  }
+  50% {
+    color: #eee;
+  }
+  100% {
+    color: ${props => props.theme.main};
+  }
+`;
+
 export const CharSpan = styled.span`
+    color: ${props => props.theme.main};
     ${props =>
-        props.$toggleColor
-            ? css`
-                  color: #eee;
-              `
-            : css`
-                  color: ${props => props.theme.main};
-              `}
-    transition: color 0.8s;
-    ${props =>
-        props.$delay &&
+        props.$toggleColor &&
         css`
-            transition-delay: ${props.$delay}s;
+            // fadeIn 애니메이션을 적용합니다.
+            // 애니메이션은 총 1.6초 동안 진행되며, props.$delay만큼 지연됩니다.
+            // 처음 0.8초 동안은 #eee로 바뀌고, 다음 0.8초 동안은 다시 원래 색상으로 돌아갑니다.
+            animation: ${fadeIn} 1.6s ${props.$delay}s;
         `}
 `;

3710d890a67295465bdb8d225d6fd532f7160fff
wooluck210 <54842493+wooluck210@users.noreply.github.com>
2023-12-14 14:11:35 +0900

    :recycle: Update select 코드 수정
    select 코드 간소화

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import * as S from './Home.styled';
-import logoImg from '../../assets/images/Logo.svg';
-import Article from './Article';
-import Slide from './Slide';
-import usePageHandler from '../../hooks/usePageHandler';
-import { getAllPostApi } from '../../service/post_service';
-import Loader from '../../components/Loading/Loader';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+
+import { getAllPostApi } from '../../service/post_service';
+
+import Loader from '../../components/Loading/Loader';
+import Slide from './Slide';
+import Article from './Article';
+
+import usePageHandler from '../../hooks/usePageHandler';
+import logoImg from '../../assets/images/Logo.svg';
 
 const Home = () => {
     const token = sessionStorage.getItem('Token');
@@ -14,27 +16,27 @@ const Home = () => {
 
     usePageHandler('image', logoImg);
 
-    const { data, error, isLoading } = useQuery({
+    const {
+        data: articles,
+        error,
+        isLoading,
+    } = useQuery({
         queryKey: ['getAllPosts', token],
         queryFn: () => getAllPostApi(token),
-        select: dataAll => {
-            let result = dataAll?.posts?.filter(post =>
-                post.content?.includes('"deskoration"'),
-            );
-
-            if (category) {
-                return result.filter(article => {
-                    const content = JSON.parse(article.content);
-                    return content.deskoration.productItems.some(
-                        item => item.detail.category === category,
-                    );
-                });
-            } else {
-                return result;
-            }
-        },
+        select: data =>
+            category
+                ? data?.posts
+                      ?.filter(post => post.content?.includes('"deskoration"'))
+                      .filter(article => {
+                          const content = JSON.parse(article.content);
+                          return content.deskoration.productItems.some(
+                              item => item.detail.category === category,
+                          );
+                      })
+                : data?.posts?.filter(post =>
+                      post.content?.includes('"deskoration"'),
+                  ),
     });
-    console.log(data);
 
     if (isLoading) {
         return <Loader />;
@@ -53,7 +55,7 @@ const Home = () => {
     return (
         <>
             <Slide category={handleCategory} />
-            <Article articles={data} />
+            <Article articles={articles} />
         </>
     );
 };

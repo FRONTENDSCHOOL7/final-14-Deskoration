0eb360a3b8f2b23ad16bbcb1a817b7eb07b963a3
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-14 17:29:47 +0900

    :rewind: 리팩토링한 코드로 다시 업데이트
    #165 머지 시 이전 코드로 변경되어 리팩토링 이후 코드로 재업데이트

--- src/pages/Profile/ProfileUpload.jsx
+++ src/pages/Profile/ProfileUpload.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './ProfileUpload.styled';
 import basicImg from '../../assets/images/basicImg.png';
 import { Input } from '../../components/Input/Input';
-import { WarningMsg } from '../../components/Input/WarningMsg';
 import { uploadImgApi } from '../../service/img_service';
 import {
     authLoginApi,
@@ -16,6 +15,7 @@ import {
 import GradientButton from '../../components/GradientButton/GradientButton';
 import { useLocation } from 'react-router';
 import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import imageCompression from 'browser-image-compression';
 import usePageHandler from '../../hooks/usePageHandler';
 
@@ -29,20 +29,33 @@ export const ProfileUpload = () => {
     const noImage = 'Ellipse.png';
 
     const [photoURL, setPhotoURL] = useState(basicImg);
-    const [existID, setExistID] = useState(null);
     const [isImageAdded, setIsImageAdded] = useState(false);
-    const [warnUserName, setWarnUserName] = useState(false);
-    const [warnID, setWarnID] = useState(false);
     const [file, setFile] = useState();
     const { emailValue, passwordValue } = useLocation().state || {};
     const [profileData, setProfileData] = useState(null);
 
-    const userNameEl = useRef(null);
-    const idEl = useRef(null);
-    const introEl = useRef(null);
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        setFocus,
+        formState: { errors, isValid },
+    } = useForm({
+        mode: 'onSubmit',
+        defaultValues: {
+            userName: '',
+            userID: '',
+            intro: '',
+        },
+    });
 
     // 헤더에 문구 넣기
     usePageHandler('text', '프로필 설정');
+
+    // 초기 렌더링 시에 포커스
+    useEffect(() => {
+        setFocus('userName');
+    }, [setFocus]);
 
     // 프로필 편집일 경우, API 호출해서 데이터 받아오기
     useEffect(() => {
@@ -58,15 +71,16 @@ export const ProfileUpload = () => {
                     const userNameValue = data.user.username;
                     const idValue = data.user.accountname;
                     const introValue = data.user.intro;
-                    userNameEl.current.value = userNameValue;
-                    idEl.current.value = idValue;
-                    introEl.current.value = introValue;
+
+                    setValue('userName', userNameValue);
+                    setValue('userID', idValue);
+                    setValue('intro', introValue);
                 })
                 .catch(error => {
                     console.error('API 요청 중 오류 발생: ', error);
                 });
         }
-    }, [token]);
+    }, [editPage, setValue, token]);
 
     // 업로드한 이미지 url 저장
     const handleUploadImg = async event => {
@@ -114,78 +128,44 @@ export const ProfileUpload = () => {
         setPhotoURL(basicImg);
     };
 
+    // 계정 ID 검사
+    const validateUserID = id => {
+        return new Promise((resolve, reject) => {
+            validAccountNameApi(id)
+                .then(result => {
+                    if (result.message === '사용 가능한 계정ID 입니다.') {
+                        resolve(true); // 유효한 경우 true 반환
+                    } else if (
+                        result.message === '이미 가입된 계정ID 입니다.'
+                    ) {
+                        resolve(false); // 이미 존재하는 경우 false 반환
+                    } else {
+                        reject(new Error(result.message)); // 예외 처리
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                    reject(error); // 에러 처리
+                });
+        });
+    };
+
     // submit 함수
-    const dataSubmit = async event => {
-        event.preventDefault();
-
-        const userNameValue = userNameEl.current.value;
-        const idValue = idEl.current.value;
-        const introValue = introEl.current.value;
-
-        // 유효성 검사
-        const checkValidUserName = userName => {
-            return userName.length >= 2 && userName.length <= 10;
-        };
-        const checkValidID = id => {
-            const regex = /^[a-zA-Z0-9._]+$/;
-            return id.length >= 2 && regex.test(id);
-        };
-
-        const validID = checkValidID(idValue);
-        const validUserName = checkValidUserName(userNameValue);
-
-        // 사용자 이름에 따른 state 변경
-        setWarnUserName(!validUserName);
-
-        // 계정 ID 검사
-        const validateAccountName = id => {
-            return new Promise((resolve, reject) => {
-                if (!checkValidID(id)) {
-                    setWarnID(true);
-                    resolve(false); // 유효하지 않은 경우 false 반환
-                } else {
-                    validAccountNameApi(id)
-                        .then(result => {
-                            if (
-                                result.message === '사용 가능한 계정ID 입니다.'
-                            ) {
-                                setExistID(false);
-                                resolve(true); // 유효한 경우 true 반환
-                            } else if (
-                                result.message === '이미 가입된 계정ID 입니다.'
-                            ) {
-                                setExistID(true);
-                                resolve(false); // 이미 존재하는 경우 false 반환
-                            } else {
-                                reject(new Error(result.message)); // 예외 처리
-                            }
-                        })
-                        .catch(error => {
-                            console.error(error);
-                            reject(error); // 에러 처리
-                        });
-                }
-            });
-        };
-
-        const isIDValid = await validateAccountName(idValue);
-
-        // 유효성 검사를 통과하면 post 요청
-        // 회원 가입 시 초기 프로필 설정일 경우
-        if (isIDValid && validUserName && !editPage) {
+    const dataSubmit = async data => {
+        //회원 가입 시 초기 프로필 설정일 경우
+        if (isValid && !editPage) {
             const userData = {
-                username: userNameValue,
+                username: data.userName,
                 email: emailValue,
                 password: passwordValue,
-                accountname: idValue,
-                intro: introValue,
+                accountname: data.userID,
+                intro: data.intro,
                 image: !file ? baseURL + noImage : baseURL + file,
             };
             authSignUpApi(userData)
                 .then(result => {
                     if (result.message === '회원가입 성공') {
                         authLoginApi(emailValue, passwordValue).then(data => {
-                            console.log(data);
                             sessionStorage.setItem('Token', data.user.token);
                             sessionStorage.setItem(
                                 'AccountName',
@@ -203,12 +183,12 @@ export const ProfileUpload = () => {
                 });
         }
         // 프로필 편집일 경우
-        else if (isIDValid && validUserName && editPage) {
+        else if (isValid && editPage) {
             const userData = {
                 user: {
-                    username: userNameValue,
-                    accountname: idValue,
-                    intro: introValue,
+                    username: data.userName,
+                    accountname: data.userID,
+                    intro: data.intro,
                     image:
                         !file && photoURL === basicImg
                             ? baseURL + noImage
@@ -217,13 +197,12 @@ export const ProfileUpload = () => {
                             : baseURL + file,
                 },
             };
-
             updateProfileApi(token, userData)
                 .then(result => {
                     if (result.message === '이미 사용중이 계정 ID입니다.') {
                         throw new Error(result.message);
                     } else {
-                        sessionStorage.setItem('AccountName', idValue);
+                        sessionStorage.setItem('AccountName', data.userID);
                         navigate('/profile');
                     }
                 })
@@ -236,7 +215,7 @@ export const ProfileUpload = () => {
     return (
         <section>
             <S.ProfileContainer>
-                <form onSubmit={dataSubmit}>
+                <form onSubmit={handleSubmit(dataSubmit)}>
                     <S.ProfileImgBox>
                         <S.ProfileImg src={photoURL} alt="프로필 이미지" />
                         {isImageAdded && (
@@ -258,35 +237,52 @@ export const ProfileUpload = () => {
                     </S.ProfileImgBox>
                     <S.InputBox>
                         <Input
-                            label="사용자 이름"
-                            inputRef={userNameEl}
-                            warning={warnUserName}
+                            label={'사용자 이름'}
+                            id={'userName'}
+                            error={errors.userName}
                             placeholder={'2~10자 이내여야 합니다.'}
+                            register={register}
+                            registerOptions={{
+                                required: '필수 정보를 입력하세요.',
+                                minLength: {
+                                    value: 2,
+                                    message: '2~10자 이내여야 합니다.',
+                                },
+                                maxLength: {
+                                    value: 10,
+                                    message: '2~10자 이내여야 합니다.',
+                                },
+                            }}
                         />
-                        {warnUserName && (
-                            <WarningMsg msg={'2~10자 이내여야 합니다.'} />
-                        )}
                         <Input
-                            label="계정 ID"
-                            inputRef={idEl}
-                            warning={warnID || existID}
+                            label={'계정 ID'}
+                            id={'userID'}
+                            error={errors.userID}
                             placeholder={
                                 '영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
                             }
+                            register={register}
+                            registerOptions={{
+                                required: '필수 정보를 입력하세요.',
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._]+$/,
+                                    message:
+                                        '영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.',
+                                },
+                                validate: async value => {
+                                    const result = await validateUserID(value);
+                                    return (
+                                        result || '이미 존재하는 계정 ID입니다.'
+                                    );
+                                },
+                            }}
                         />
-                        {warnID ? (
-                            <WarningMsg
-                                msg={
-                                    '영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
-                                }
-                            />
-                        ) : existID ? (
-                            <WarningMsg msg="이미 가입된 계정ID 입니다." />
-                        ) : null}
                         <Input
-                            label="소개"
-                            inputRef={introEl}
+                            label={'소개'}
+                            id={'intro'}
+                            error={errors.intro}
                             placeholder={'자신을 소개해주세요.'}
+                            register={register}
                         />
                     </S.InputBox>
                     <GradientButton

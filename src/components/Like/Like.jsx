93e34401c2250785e01b8b8a0d2fee6fc0808f18
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-19 18:11:20 +0900

    :recycle: Add like component
    #208 좋아요 기능과 버튼을 컴포넌트화

---
+++ src/components/Like/Like.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SocialButton from '../SocialButton/SocialButton';
+import { useLikeUpdate } from '../../hooks/useLikeUpdate';
+
+const Like = ({ queryKey, isLike, likeCount, mutationParams }) => {
+    const likeMutation = useLikeUpdate(queryKey, true);
+    const cancelLikeMutation = useLikeUpdate(queryKey, false);
+
+    return (
+        <>
+            <SocialButton
+                type={'like'}
+                onClick={() =>
+                    !isLike
+                        ? likeMutation.mutate(mutationParams)
+                        : cancelLikeMutation.mutate(mutationParams)
+                }
+                isLike={isLike}
+                likeCount={likeCount}
+            />
+        </>
+    );
+};
+
+export default Like;

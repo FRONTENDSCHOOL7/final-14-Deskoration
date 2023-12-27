f94fc44f1a024d79239b57eb617cad48ab2c759b
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-27 10:48:18 +0900

    :sparkles: Add NoContents Component
    #229 프로필, 피드, 팔로우 등에서 데이터가 없을 경우 쓰일 컴포넌트 생성

--- src/components/NoContents/NoContents.jsx
+++ src/components/NoContents/NoContents.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './NoContents.styled';
 
-const NoContents = () => {
-    return <div>컨텐츠없음</div>;
+const NoContents = props => {
+    const { title, subTitle, link, btnTxt } = props;
+    return (
+        <S.NoContentsContainer>
+            <p>
+                <span>{title}</span>
+                <span>{subTitle}</span>
+            </p>
+            {!link ? null : (
+                <S.LinkBox>
+                    <Link to={link}>{btnTxt}</Link>
+                </S.LinkBox>
+            )}
+        </S.NoContentsContainer>
+    );
 };
 
 export default NoContents;

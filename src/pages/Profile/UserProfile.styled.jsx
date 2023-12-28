dc47fe253bb630766092f58a16acf89a9f106ae8
HyeonJin <ocn4091@naver.com>
2023-12-29 04:48:36 +0900

    :recycle: Refactor 프로필 컴포넌트 통일
    #231 프로필 컴포넌트와 유저 프로필 컴포넌트를 통합함

--- src/pages/Profile/UserProfile.styled.jsx
+++ src/pages/Profile/UserProfile.styled.jsx
@@ -1,133 +1,133 @@
-import styled from 'styled-components';
-import theme from '../../styles/theme';
-import { ReactComponent as Backward } from '../../assets/images/Backward.svg';
-import { ReactComponent as More } from '../../assets/images/Dots_vertical.svg';
+// import styled from 'styled-components';
+// import theme from '../../styles/theme';
+// import { ReactComponent as Backward } from '../../assets/images/Backward.svg';
+// import { ReactComponent as More } from '../../assets/images/Dots_vertical.svg';
 
-export const Backwardicon = styled(Backward)``;
+// export const Backwardicon = styled(Backward)``;
 
-export const ProfileContainer = styled.div`
-    width: 100%;
-`;
+// export const ProfileContainer = styled.div`
+//     width: 100%;
+// `;
 
-export const SocialButtonBox = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-`;
+// export const SocialButtonBox = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     gap: 10px;
+// `;
 
-export const UserInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+// export const UserInfo = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
 
-    img {
-        width: 100px;
-        height: 100px;
-        border-radius: 100%;
-        object-fit: cover;
-        border: 1px solid ${props => props.theme.border};
-    }
+//     img {
+//         width: 100px;
+//         height: 100px;
+//         border-radius: 100%;
+//         object-fit: cover;
+//         border: 1px solid ${props => props.theme.border};
+//     }
 
-    div {
-        display: flex;
-        flex-direction: column;
-        padding-left: 10px;
-        flex-grow: 1;
-        text-align: center;
-    }
+//     div {
+//         display: flex;
+//         flex-direction: column;
+//         padding-left: 10px;
+//         flex-grow: 1;
+//         text-align: center;
+//     }
 
-    div > p:first-child {
-        font-size: ${theme.fontSize.md};
-        font-family: 'PreBold', sans-serif;
-        margin: 10px 0;
-    }
-`;
+//     div > p:first-child {
+//         font-size: ${theme.fontSize.md};
+//         font-family: 'PreBold', sans-serif;
+//         margin: 10px 0;
+//     }
+// `;
 
-export const UserDataList = styled.div`
-    margin: 15px 0;
-    display: flex;
-    justify-content: center;
+// export const UserDataList = styled.div`
+//     margin: 15px 0;
+//     display: flex;
+//     justify-content: center;
 
-    > * {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        flex-grow: 1;
-        padding: 4px;
-    }
+//     > * {
+//         display: flex;
+//         flex-direction: column;
+//         justify-content: center;
+//         align-items: center;
+//         flex-grow: 1;
+//         padding: 4px;
+//     }
 
-    a > p {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+//     a > p {
+//         display: flex;
+//         flex-direction: column;
+//         justify-content: center;
+//         align-items: center;
+//     }
 
-    a:nth-child(2) {
-        border-left: 1px solid ${theme.border};
-        border-right: 1px solid ${theme.border};
-    }
+//     a:nth-child(2) {
+//         border-left: 1px solid ${theme.border};
+//         border-right: 1px solid ${theme.border};
+//     }
 
-    p > span:first-child {
-        font-family: 'PreBold', sans-serif;
-        font-size: ${theme.fontSize.md};
-        margin-bottom: 8px;
-    }
+//     p > span:first-child {
+//         font-family: 'PreBold', sans-serif;
+//         font-size: ${theme.fontSize.md};
+//         margin-bottom: 8px;
+//     }
 
-    p > span:nth-child(2) {
-        color: ${theme.subFont};
-    }
-`;
+//     p > span:nth-child(2) {
+//         color: ${theme.subFont};
+//     }
+// `;
 
-export const UserPostings = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(145px, 1fr));
-    grid-gap: 10px;
-    margin-top: 20px;
-    color: ${({ theme }) => theme.mainFont};
+// export const UserPostings = styled.div`
+//     width: 100%;
+//     display: grid;
+//     grid-template-columns: repeat(auto-fill, minmax(145px, 1fr));
+//     grid-gap: 10px;
+//     margin-top: 20px;
+//     color: ${({ theme }) => theme.mainFont};
 
-    @media screen and (min-width: 600px) {
-        grid-template-columns: repeat(3, 1fr);
-    }
+//     @media screen and (min-width: 600px) {
+//         grid-template-columns: repeat(3, 1fr);
+//     }
 
-    a {
-        aspect-ratio: 1/1;
-        max-width: 280px;
-        max-height: 280px;
-    }
+//     a {
+//         aspect-ratio: 1/1;
+//         max-width: 280px;
+//         max-height: 280px;
+//     }
 
-    img {
-        width: 100%;
-        height: 100%;
-        aspect-ratio: 1 / 1;
-        object-fit: cover;
-        transition: border 0.1s ease;
-        &:hover {
-            border: 3px solid ${theme.main};
-        }
-        border: 1px solid ${theme.border};
-        border-radius: 10px;
-    }
-`;
+//     img {
+//         width: 100%;
+//         height: 100%;
+//         aspect-ratio: 1 / 1;
+//         object-fit: cover;
+//         transition: border 0.1s ease;
+//         &:hover {
+//             border: 3px solid ${theme.main};
+//         }
+//         border: 1px solid ${theme.border};
+//         border-radius: 10px;
+//     }
+// `;
 
-export const MoreButton = styled.button`
-    width: 30px;
-    height: 30px;
-    position: absolute;
-    top: 15px;
-    right: 20px;
-    transform: rotate(90deg);
-`;
+// export const MoreButton = styled.button`
+//     width: 30px;
+//     height: 30px;
+//     position: absolute;
+//     top: 15px;
+//     right: 20px;
+//     transform: rotate(90deg);
+// `;
 
-export const MoreIcon = styled(More)`
-    width: 25px;
-    height: 25px;
-`;
+// export const MoreIcon = styled(More)`
+//     width: 25px;
+//     height: 25px;
+// `;
 
-export const ToggleButton = styled.button`
-    color: ${({ theme }) => theme.subFont};
-    padding-left: 4px;
-    font-weight: 700;
-`;
+// export const ToggleButton = styled.button`
+//     color: ${({ theme }) => theme.subFont};
+//     padding-left: 4px;
+//     font-weight: 700;
+// `;

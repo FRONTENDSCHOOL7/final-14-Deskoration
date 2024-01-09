a30d96b23d25a8d9a5a7d7a971d869e24e3d13e5
yen <l.yennie.j@gmail.com>
2024-01-09 11:06:43 +0900

    :coffin: Remove dead code.

--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -1,10 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import axiosInstance from '../service/axiosInstance';
 
 const PublicRoutePage = () => {
-    // const authenticated =
-    //     axiosInstance.defaults.headers.Authorization !== 'Bearer null';
-    const accessToken = window.sessionStorage.getItem('Token');
     const authenticated = window.sessionStorage.getItem('Token');
 
     return authenticated ? (
